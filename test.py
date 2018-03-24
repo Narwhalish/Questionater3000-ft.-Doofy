10fb419171247565b03509b614794db42c7e8790
Narwhalish <bryao@ctemc.org>
2018-03-24 13:22:28 -0400

    Remove Gadfly

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-print os.getcwd()
-
-from TheGadflyProject.gadfly.gap_fill_generator import GapFillGenerator
-g = GapFillGenerator("This is my input texts. \'It will probably be much longer than this, hopefully\' said Daniel.")
-for q in g.questions: print(q)
