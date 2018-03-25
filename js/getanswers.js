6e776bc71045a961e9fe03942fdecad90b56551e
Daniel Shulman <sivanshulman@gmail.com>
2018-03-24 22:56:48 -0400

    Form True/False
    Added True/False Select Menus

--- js/getanswers.js
+++ js/getanswers.js
@@ -1,18 +1,14 @@
 function storeAnswers(){
   console.log('hello')
-  // var ans1 = document.getElementById('ans1')
-  // var ans2 = document.getElementById('ans2')
-  // var ans3 = document.getElementById('ans3')
-  // var ans4 = document.getElementById('ans4')
-  // var ans5 = document.getElementById('ans5')
-  //
-  // console.log(ans1)
-  // console.log(ans2)
-  // console.log(ans3)
-  // console.log(ans4)
-  // console.log(ans5)
-}
+  var ans1 = document.getElementById('ans1')
+  var ans2 = document.getElementById('ans2')
+  var ans3 = document.getElementById('ans3')
+  var ans4 = document.getElementById('ans4')
+  var ans5 = document.getElementById('ans5')
 
-function jeff(){
-  console.log('jeff')
+  console.log(ans1)
+  console.log(ans2)
+  console.log(ans3)
+  console.log(ans4)
+  console.log(ans5)
 }
