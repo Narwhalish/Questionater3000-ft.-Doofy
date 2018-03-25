9bcbf8dd6b6a0a2156aae189f92ac89c42a851ae
Daniel Shulman <sivanshulman@gmail.com>
2018-03-25 08:37:55 -0400

    Answer Checker
    Started setup for checking the answers

--- js/getanswers.js
+++ js/getanswers.js
@@ -1,14 +1,5 @@
-function storeAnswers(){
-  console.log('hello')
-  var ans1 = document.getElementById('ans1')
-  var ans2 = document.getElementById('ans2')
-  var ans3 = document.getElementById('ans3')
-  var ans4 = document.getElementById('ans4')
-  var ans5 = document.getElementById('ans5')
-
-  console.log(ans1)
-  console.log(ans2)
-  console.log(ans3)
-  console.log(ans4)
-  console.log(ans5)
+function revealAns(){
+  console.log('testing')
 }
+
+revealAns()
