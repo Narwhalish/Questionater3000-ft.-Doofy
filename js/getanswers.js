bba60525bfe5b08025e5ecf6f8978cfb1a08102e
Daniel Shulman <sivanshulman@gmail.com>
2018-03-25 08:45:03 -0400

    Get Answers
    Put in placeholder for get answers file

--- js/getanswers.js
+++ js/getanswers.js
@@ -1,5 +1,12 @@
-function revealAns(){
-  console.log('testing')
-}
-
-revealAns()
+$document.ready(function(){
+  $( "<p>Test</p>" ).insertAfter( "#ans1" );
+  $( "<p>Test</p>" ).insertAfter( "#ans2" );
+  $( "<p>Test</p>" ).insertAfter( "#ans3" );
+  $( "<p>Test</p>" ).insertAfter( "#ans4" );
+  $( "<p>Test</p>" ).insertAfter( "#ans5" );
+  $( "<p>Test</p>" ).insertAfter( "#true-false1" );
+  $( "<p>Test</p>" ).insertAfter( "#true-false2" );
+  $( "<p>Test</p>" ).insertAfter( "#true-false3" );
+  $( "<p>Test</p>" ).insertAfter( "#true-false4" );
+  $( "<p>Test</p>" ).insertAfter( "#true-false5" );
+});
