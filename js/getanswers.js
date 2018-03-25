cc589a109df19c219b3be62020d284612c753601
Daniel Shulman <sivanshulman@gmail.com>
2018-03-25 09:43:39 -0400

    Show Answers

--- js/getanswers.js
+++ js/getanswers.js
@@ -1,4 +1,5 @@
-$document.ready(function(){
+function showAnswers(){
+  console.log('hello');
   $( "<p>Test</p>" ).insertAfter( "#ans1" );
   $( "<p>Test</p>" ).insertAfter( "#ans2" );
   $( "<p>Test</p>" ).insertAfter( "#ans3" );
@@ -9,4 +10,4 @@ $document.ready(function(){
   $( "<p>Test</p>" ).insertAfter( "#true-false3" );
   $( "<p>Test</p>" ).insertAfter( "#true-false4" );
   $( "<p>Test</p>" ).insertAfter( "#true-false5" );
-});
+}
